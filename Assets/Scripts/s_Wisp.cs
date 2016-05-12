109a84281569699049c469361e5ff8724bd3ede1
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-05-12 03:20:55 -0600

    Wisp is in, just needs to shoot stuff

---
+++ Assets/Scripts/s_Wisp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Wisp : MonoBehaviour {
+
+    public Transform book;
+    public float yOffset;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // put the wisp above the book always
+        transform.position = new Vector3(book.transform.position.x, (book.position.y + 2.8f) + yOffset * Mathf.Sin(speed * Time.time), book.transform.position.z);
+	}
+}
