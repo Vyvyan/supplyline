592690068b1b9a009c6d9de82a4d01575e936986
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-06-07 01:26:11 -0600

    Added in shipping crates, there's a base one to copy and modify for each item shipped, only one currently is an axe create

---
+++ Assets/Scripts/s_CrateParts.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_CrateParts : MonoBehaviour {
+
+    FixedJoint joint;
+    public GameObject crateSide;
+    public GameObject payLoad;
+
+	// Use this for initialization
+	void Start ()
+    {
+        joint = gameObject.GetComponent<FixedJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Axe")
+        {
+            Instantiate(crateSide, gameObject.transform.position, gameObject.transform.rotation);
+            // if any of the crate sides break, we need to make the payload a physics object 
+            if (payLoad)
+            {
+                payLoad.GetComponent<Rigidbody>().isKinematic = false;
+                payLoad.transform.parent = null;
+            }
+            Destroy(gameObject);
+        }
+        else if (other.relativeVelocity.magnitude >= 30)
+        {
+            Instantiate(crateSide, gameObject.transform.position, gameObject.transform.rotation);
+            // if any of the crate sides break, we need to make the payload a physics object
+            if (payLoad)
+            {
+                payLoad.GetComponent<Rigidbody>().isKinematic = false;
+                payLoad.transform.parent = null;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
