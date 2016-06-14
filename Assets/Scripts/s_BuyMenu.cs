f8f159da9809a3b369ad0b9c77f1af79bec48319
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-06-14 01:50:39 -0600

    first steps for the buy menu implemented

---
+++ Assets/Scripts/s_BuyMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_BuyMenu : MonoBehaviour {
+
+    public enum MenuState {start, defense, resourceGather, misc};
+    public MenuState menuState;
+    public MenuState menuStateLastFrame;
+
+    public GameObject startGroup, defenseGroup, resourceGatherGroup, miscGroup;
+
+	// Use this for initialization
+	void Start ()
+    {
+        menuState = MenuState.start;
+        defenseGroup.active = false;
+        resourceGatherGroup.active = false;
+        miscGroup.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (menuState == MenuState.start)
+        {
+            if (startGroup.active == false)
+            {
+                startGroup.active = true;
+                defenseGroup.active = false;
+                resourceGatherGroup.active = false;
+                miscGroup.active = false;
+            }
+        }
+	}
+}
