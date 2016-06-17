b24d441a7dd7c43a344434f4b62f90a7e989a919
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-06-16 21:49:26 -0600

    idk more stuff for the transport thing, doesn't quite work yet

---
+++ Assets/Scripts/s_ShippingCatapult.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_ShippingCatapult : MonoBehaviour {
+
+    public GameObject axeCrate;
+    public GameObject shippingObject;
+
+    public GameObject target;
+    public float power;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        target = GameObject.FindGameObjectWithTag("BuyScreen");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.I))
+        {
+
+            shippingObject = Instantiate(axeCrate, gameObject.transform.position, Quaternion.identity) as GameObject;
+            Rigidbody rb = shippingObject.transform.GetChild(0).GetComponent<Rigidbody>();
+            Vector3 direction = target.transform.position - gameObject.transform.position;
+
+            // fire at the buy menu, with a power based on the distance between the catapult and the tower
+            rb.AddForce(new Vector3(direction.x, 0, direction.z) * (power * (direction.magnitude)), ForceMode.VelocityChange);
+        }
+	}
+}
---
+++ Assets/Scripts/s_TestLauncher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_TestLauncher : MonoBehaviour {
+
+    public GameObject launchTarget;
+    public float force;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.I))
+        {
+            launchTarget.GetComponent<Rigidbody>().AddForce(Vector3.up * force, ForceMode.VelocityChange);
+        }
+	}
+}
