18a1ca155f62912cb26c06f556274819b54bf521
Trevor Vaughn <TrevorJVaughn@gmail.com>
2016-07-14 19:30:31 -0600

    Fish Drill in the game, fish in the game, including fish flopping

---
+++ Assets/Scripts/s_Fish.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Fish : MonoBehaviour {
+
+    //this script controls the fish flop fish behavior
+
+    float fishLifeTime;
+    Rigidbody rb;
+    bool canFlop;
+    float fishFlopTimer_Current, fishFlopTimer_Max;
+
+	// Use this for initialization
+	void Start ()
+    {
+        canFlop = false;
+        rb = gameObject.GetComponent<Rigidbody>();
+        fishLifeTime = 20;
+        NewFishFlopTimer();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (fishLifeTime > 0)
+        {
+            // countdown
+            fishLifeTime -= Time.deltaTime;
+
+            if (fishFlopTimer_Current > 0)
+            {
+                fishFlopTimer_Current -= Time.deltaTime;
+            }
+            else
+            {
+                if (canFlop)
+                {
+                    rb.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-2, 2), 3, Random.Range(-2, 2)), ForceMode.Impulse);
+                    rb.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(Random.Range(0, 100), Random.Range(0, 100), Random.Range(0, 100)));
+                }
+                NewFishFlopTimer();
+            }
+        }
+	}
+
+    void NewFishFlopTimer()
+    {
+        fishFlopTimer_Max = Random.Range(.2f, 1f);
+        fishFlopTimer_Current = fishFlopTimer_Max;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        canFlop = true;
+    }
+}
---
+++ Assets/Scripts/s_FishDrill.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_FishDrill : MonoBehaviour {
+
+    public GameObject fish, drill, spawnLocation;
+    bool onIce;
+
+    public float fishTimer_Current, fishTimer_Max;
+
+    float spinAmount;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetNewFishTimer();
+        spinAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // spin drill
+        drill.transform.Rotate(Vector3.down * spinAmount);
+
+        if (spinAmount < 0) { spinAmount = 0; }
+
+        if (onIce)
+        {
+            // rev up on the drill
+            if (spinAmount < 30)
+            {
+                spinAmount += (3 * Time.deltaTime);
+            }
+
+            // if we are above zero on the counter, then count down
+            if (fishTimer_Current > 0)
+            {
+                fishTimer_Current -= (1 * Time.deltaTime);
+            }
+            // else we spawn a fish and get a new timer
+            else
+            {
+                // spawn fish
+                GameObject temp = Instantiate(fish, spawnLocation.transform.position, Quaternion.identity) as GameObject;
+                // add a force to the fish
+                temp.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-5, 5), 7, Random.Range(-5, 5)), ForceMode.Impulse);
+                temp.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(Random.Range(0, 100), Random.Range(0, 100), Random.Range(0, 100)));
+                GetNewFishTimer();
+            }
+        }
+        // not on ice
+        else
+        {
+            // rev down on the drill
+            if (spinAmount > 0)
+            {
+                spinAmount -= (8 * Time.deltaTime);
+            }
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ice")
+        {
+            onIce = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ice")
+        {
+            onIce = false;
+        }
+    }
+
+    void GetNewFishTimer()
+    {
+        fishTimer_Max = Random.Range(5f, 90f);
+        fishTimer_Current = fishTimer_Max;
+    }
+}
